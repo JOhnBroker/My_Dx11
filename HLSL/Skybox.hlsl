ae365398bee4b696f4cfc4cf123f0d859d456cde
chenxinyi1 <chenxinyi1@kingsoft.com>
2022-08-09 18:19:50 +0800

    Add some shader files for rendering shadow map.

---
+++ HLSL/Skybox.hlsl
@@ -0,0 +1,56 @@
+#ifndef SKYBOX_HLSL
+#define SKYBOX_HLSL
+
+uniform matrix g_ViewProj;
+
+struct VertexPosNormalTex
+{
+    float3 PosL : POSITION;
+    float3 normal : NORMAL;
+    float2 texCoord : TEXCOORD;
+};
+
+//--------------------------------------------------------------------------------------
+// 后处理, 天空盒等
+// 使用天空盒几何体渲染
+//--------------------------------------------------------------------------------------
+TextureCube<float4> g_SkyboxTexture : register(t5);
+Texture2D<float> g_DepthTexture : register(t6);
+Texture2D<float4> g_LitTexture : register(t7);
+
+SamplerState g_SamplerDiffuse : register(s0);
+
+struct SkyboxVSOut
+{
+    float4 posViewport : SV_Position;
+    float3 skyboxCoord : skyboxCoord;
+};
+
+SkyboxVSOut VS(VertexPosNormalTex vIn)
+{
+    SkyboxVSOut vOut;
+    
+    vOut.posViewport = mul(float4(vIn.PosL, 1.0f), g_WorldViewProj).xyww;
+    vOut.skyboxCoord = vIn.PosL;
+    return vOut;
+}
+
+float4 PS(SkyboxVSOut pIn) : SV_Target
+{
+    uint2 coord = pIn.posViewport.xy;
+    
+    float3 lit = float3(0.0f, 0.0f, 0.0f);
+    float depth = g_DepthTexture[coord];
+    if (depth >= 1.0f)
+    {
+        lit += g_SkyboxTexture.Sample(g_SamplerDiffuse, pIn.skyboxCoord).rgb;
+    }
+    else
+    {
+        lit += g_LitTexture[coord].xy;
+    }
+    
+    return float4(lit, 1.0f);
+}
+
+#endif
